12e2dbceecb7c82d8c90139183e0730214c5769b
Akash Srivastava <aksaps1@gmail.com>
2019-08-16 16:36:43 +0530

    solved prob 5 dynamic delay

--- src/api/prodapis/handlers.js
+++ src/api/prodapis/handlers.js
@@ -1,4 +1,10 @@
 const csvtojson = require("csvtojson");
+const ip = require('ip');
+const {promisify} = require('util');
+const redis = require("redis");
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 
 let adder = (sum, element) => {
@@ -7,6 +13,46 @@ let adder = (sum, element) => {
   });
 
   return p;
+}
+
+//Returns client IP address
+let getClientIp = (req) => {
+  var ipAddress;
+  // The request may be forwarded from local web server.
+  var forwardedIpsStr = req.headers['x-forwarded-for']; 
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    // If request was not forwarded
+    ipAddress = req.info.remoteAddress;
+  }
+
+  return ipAddress;
+}
+
+//calcDelay calculates the delay time after which res will send to client
+let calcDelay = (totalHits) => {
+  let promise = new Promise ((resolve) => {
+    let hit = parseInt(totalHits);
+
+    resolve((hit - 1) + hit);
+  });
+  
+  return promise;
+}
+
+//calcResponse to get res which will send after delay 
+let calcResponse = (totalHits, delay) => {
+  let promise = new Promise ((resolve) => {
+    setTimeout(() => resolve(2 ** totalHits), delay * 1000);
+  });
+
+  return promise;
 }
 
 export let loop = async (request, h) => {
@@ -41,4 +87,34 @@ export let csvToJSON = async (request) => {
     console.error(err);
   }
 };
+
+export let dynamicDelay = async (request) => {
+  try {
+    let ipAdd = getClientIp(request);
+    let key = ip.toLong(ipAdd);
+
+    //Get val of key from redis
+    let value = await getAsync(key);
+
+    if (!value) {
+      await setAsync(key, 1, 'EX', 5 * 60);
+      value = 1;
+    }
+
+    //Each time val of key increase by 1 in redis
+    client.incr(key);
+
+    let delay = await calcDelay(value);
+    console.log(`delay ${delay} second`);
+    //Promise will resolve after a delay
+    let response = await calcResponse(value, delay);
+    
+    return response;
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
+
+
 
--- src/api/prodapis/index.js
+++ src/api/prodapis/index.js
@@ -11,6 +11,27 @@ let routes = [
   	method: 'POST',
     path: '/csv2json',
     handler: handlers.csvToJSON
+  },
+  /*
+  Each time a client make a req, this will send res (First res: 2, Sec res: 4, Third res: 8.....) after a certain delay
+  (First req: 1s delay, Second req: 3s delay,
+  Third req: 5s delay.....).
+  Using client IP and saving it in redis as key after converting it to long int.
+  Saving total number of reqs made from a particular IP as value of key.
+  Each IP saved as key would auto expire after 5 min since the first req made to that IP.
+  We can also get number of hits came from a particular IP, using IP add.
+
+  Suppose if at exact same time two or more req is send to server from a particular IP, then the responses would not send back to 
+  client at the same time, responses would send one after another since we are performing atomic operation on redis (i.e client.incr)
+  key increment.
+
+  It should work normally whether it is called by 10 clients or thousand of clients since nodejs is single threaded and code written 
+  for this is non blocking, asynchronous.
+  */
+  {
+    method: 'GET',
+    path: '/dynamicdelay',
+    handler: handlers.dynamicDelay
   }
 ];
 
